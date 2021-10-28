f2ddb3eae96a6e4bf5072d1f771b45690a40033c
MingyangYin <675496772@qq.com>
2021-10-28 18:49:38 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# import torch
+# x = torch.rand(5,3)
+# print(x)
+import torch
+# test gpu
+import time
+
+print(torch.__version__)
+print(torch.cuda.is_available())
+a = torch.randn(10000, 1000)
+b = torch.randn(1000, 2000)
+t0 = time.time()
+c = torch.matmul(a, b)
+t1 = time.time()
+print(a.device, t1 - t0, c.norm(2))
+
+device = torch.device('cuda')
+a = a.to(device)
+b = b.to(device)
+t0 = time.time()
+c = torch.matmul(a, b)
+t1 = time.time()
+print(a.device, t1 - t0, c.norm(2))
+
+t0 = time.time()
+c = torch.matmul(a, b)
+t1 = time.time()
+print(a.device, t1 - t0, c.norm(2))
+x = 10
+if x >= 20:
+    sb = 5;
+else:
+    sb = 4;
+
+print(sb)
---
+++ npy.py
@@ -0,0 +1,16 @@
+import numpy as np
+a = np.array([[1,2], [2,3] ,[3,0]])
+print(a.shape)
+print(a.shape[0])
+print(a)
+x = 100
+# for i in range(x):
+#     print(i)
+#     print(np.arange(i))
+x = np.array([[1,2],[3,4]])
+y = np.array([[5,6],[7,8]])
+
+v = np.array([9,10])
+w = np.array([11, 12])
+
+print(np.dot(v,x))
